fc3cef650fca5be65f5f9ba7ebe5fe0c717750dc
A4n9g7e2l <a4n9g7e2l@users.noreply.github.com>
2026-06-30 18:34:36 +0800

    build: add GraalVM JS 23.0.12 for js_encrypt tool

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     compileOnly("net.portswigger.burp.extender:montoya-api:2024.12")
     implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation("com.google.code.gson:gson:2.11.0")
+    implementation("org.graalvm.js:js:23.0.12")
+    implementation("org.graalvm.js:js-scriptengine:23.0.12")
 }
 
 tasks.build {
